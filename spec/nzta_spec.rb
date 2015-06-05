ee955e06065dfab9f9a84a0bf84d4d935d6869c9
Caleb <caleb.tutty@nzherald.co.nz>
2015-06-05 18:23:40 +1200

    Use RSpec for tests

--- spec/nzta_spec.rb
+++ spec/nzta_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe NZTA do
   it 'has a version number' do
-    expect(Nzta::VERSION).not_to be nil
+    expect(NZTA::VERSION).not_to be nil
   end
 
   it 'does something useful' do
