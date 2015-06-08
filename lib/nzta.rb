e784c2b83b8b1e6fcb94793a92405594399afd89
Caleb <caleb.tutty@nzherald.co.nz>
2015-06-08 19:10:59 +1200

    Add requires

--- lib/nzta.rb
+++ lib/nzta.rb
@@ -3,3 +3,11 @@ require 'nzta/version'
 module NZTA
   # Your code goes here...
 end
+
+require 'nzta/signs'
+require 'nzta/auckland_traffic_conditions'
+require 'nzta/treis'
+require 'nzta/webcams'
+# require 'nzta/location_reference'
+require 'nzta/segments'
+require 'nzta/christchurch_traffic_conditions'
