e522aaf498ba169ef51de62bba955ca7dde0b763
Caleb <caleb.tutty@nzherald.co.nz>
2016-06-01 09:57:40 +1200

    Remove unused files

--- lib/nzta.rb
+++ lib/nzta.rb
@@ -7,10 +7,8 @@ module NZTA
 end
 
 require 'nzta/signs'
-require 'nzta/auckland_traffic_conditions'
 require 'nzta/client'
 # require 'nzta/treis'
 # require 'nzta/webcams'
 # require 'nzta/location_reference'
 require 'nzta/segments'
-require 'nzta/christchurch_traffic_conditions'
