2a8c1a9767cd19d251847928c5975b089a03f15f
Caleb <caleb.tutty@nzherald.co.nz>
2016-06-01 09:58:45 +1200

    Parse XML response to a hash for segment queries and add map_segments

--- lib/nzta/client.rb
+++ lib/nzta/client.rb
@@ -13,11 +13,6 @@ module NZTA
       @http_client = setup_http_client(options[:http_client])
     end
 
-    def get_xml(url)
-      resp = http_client.get url
-      Nokogiri::XML(resp)
-    end
-
     def traffic_conditions
       get_xml 'https://infoconnect1.highwayinfo.govt.nz/ic/jbi/TrafficConditions2/REST/FeedService/'
     end
@@ -30,11 +25,68 @@ module NZTA
       get_xml 'https://infoconnect1.highwayinfo.govt.nz/ic/jbi/TrafficCameras2/REST/FeedService/'
     end
 
-    def segments
-      get_xml 'https://infoconnect1.highwayinfo.govt.nz/ic/jbi/SsdfJourney2/REST/FeedService/'
+    def segments(id = nil)
+      process_segments(get_segments(id))
+    end
+
+    def generate_segment_map(id = nil)
+      map_segments(get_segments(id))
     end
 
     private
+
+    def get_xml(url)
+      resp = http_client.get url
+      Nokogiri::XML(resp).remove_namespaces!
+    end
+
+    def get_segments(id = nil)
+      url = 'https://infoconnect1.highwayinfo.govt.nz/ic/jbi/SsdfJourney2/REST/FeedService/'
+
+      url = url + "segments?type=#{id}" if id
+      get_xml(url).css('return')
+    end
+
+    def map_segments(segments)
+      segments.map do |segment|
+        {
+          id: segment.css('id').text,
+          name: segment.css('sectionName').text,
+          sectionLength: segment.css('sectionLength').text,
+          type: {
+            groupType: segment.css('segmentType groupType').text,
+            id: segment.css('segmentType id').text,
+            name: segment.css('segmentType name').text
+          },
+          start: {
+            latitude: segment.css('startLocation latitude').text,
+            longitude: segment.css('startLocation longitude').text,
+            speedLimit: segment.css('startLocation speedLimit').text
+          },
+          end: {
+            latitude: segment.css('endLocation latitude').text,
+            longitude: segment.css('endLocation longitude').text,
+            speedLimit: segment.css('endLocation speedLimit').text
+          }
+        }
+      end
+    end
+
+    def process_segments(segments)
+      segments.map do |segment|
+        {
+          id: segment.css('id').text,
+          averageOccupancy: segment.css('averageOccupancy').text,
+          averageSpeed: segment.css('averageSpeed').text,
+          carriagewaySegmentId: segment.css('carriagewaySegmentId').text,
+          defaultSpeed: segment.css('defaultSpeed').text,
+          lastReadingTime: segment.css('lastReadingTime').text,
+          reliability: segment.css('reliability').text,
+          sectionTime: segment.css('sectionTime').text,
+          sectionName: segment.css('sectionName').text
+        }
+      end
+    end
 
     def setup_http_client(http_client)
       return http_client.new(username: @username, password: @password) unless http_client.nil?
