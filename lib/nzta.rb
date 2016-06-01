36bad0399d90c42a6ee68972c46022012fd13aec
Caleb <caleb.tutty@nzherald.co.nz>
2016-06-01 17:12:54 +1200

    Remove empty files

--- lib/nzta.rb
+++ lib/nzta.rb
@@ -6,9 +6,4 @@ module NZTA
   # Your code goes here...
 end
 
-require 'nzta/signs'
 require 'nzta/client'
-# require 'nzta/treis'
-# require 'nzta/webcams'
-# require 'nzta/location_reference'
-require 'nzta/segments'
